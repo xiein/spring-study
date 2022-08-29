9a41704ec8fb7cbac2f623aaa5d7235d67471c3a
faith <704057147@qq.com>
2022-08-29 20:05:15 +0800

    update(spring-uploading-files):添加stream 测试用例

--- spring-uploading-files/src/test/java/com/faith/SpringUploadingFilesApplicationTests.java
+++ spring-uploading-files/src/test/java/com/faith/SpringUploadingFilesApplicationTests.java
@@ -5,6 +5,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @SpringBootTest
 class SpringUploadingFilesApplicationTests {
@@ -36,6 +39,16 @@ class SpringUploadingFilesApplicationTests {
         Path absolutePath = path.resolve("demo").normalize().toAbsolutePath();
         System.out.println("parent: " + absolutePath.getParent());
         System.out.println(absolutePath);
+    }
+
+    @Test
+    void streamTest() {
+        Stream<String> stringStream = Stream.of("apple", "hello", "world", "java", "python");
+        List<String> collect = stringStream.filter(s -> s.length() > 5)
+                .collect(Collectors.toList());
+        for (String s: collect) {
+            System.out.println(s);
+        }
     }
 
 }
